4153715ca2b2129511cbb4816c4e4b046b863ea2
StanMarek <stanislawmarek21@gmail.com>
2020-11-30 16:47:59 +0100

    Add toString() in Employee

--- src/person/Employee/Employee.java
+++ src/person/Employee/Employee.java
@@ -2,6 +2,8 @@ package person.Employee;
 
 import person.Person;
 
+import java.util.Objects;
+
 public class Employee extends Person {
     private String sID;
     private String password;
@@ -23,5 +25,27 @@ public class Employee extends Person {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "sID='" + sID + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(sID, employee.sID) &&
+                Objects.equals(password, employee.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sID, password);
     }
 }
