bdf2010449cd86578f634a09842b997e00556da9
StanMarek <stanislawmarek21@gmail.com>
2021-01-15 17:42:18 +0100

    Tests of InterestCalculator completed

--- src/main/java/auto/calculators/InterestCalculator.java
+++ src/main/java/auto/calculators/InterestCalculator.java
@@ -1,11 +1,19 @@
 package auto.calculators;
 
+import java.util.concurrent.ExecutionException;
+
 public class InterestCalculator {
     private static double interest;
     private static double rate; // 0 - 1
-    public static double calculateInterestOfLoan(double amount, int time){
-        rate = 0.15;    //15%
-        interest = ((amount*rate*Math.pow(1+rate, time))/ (Math.pow(1+rate, time-1)));
-        return interest;
+
+    public static double calculateInterestOfLoan(double amount, int time) throws Exception {
+        if (amount > 0 && time > 0) {
+            rate = 0.15;    //15%
+            interest = ((amount*rate*Math.pow(1+rate, time))/ (Math.pow(1+rate, time-1)));
+            return interest;
+        }else {
+            throw new IllegalArgumentException("Values less than zero");
+        }
+
     }
 }
--- src/test/java/auto/calculators/InterestCalculatorTest.java
+++ src/test/java/auto/calculators/InterestCalculatorTest.java
@@ -1,26 +1,85 @@
 package auto.calculators;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.aggregator.AggregateWith;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class InterestCalculatorTest {
+    private double amount;
+    private int time;
 
-    private InterestCalculator calculator;
+    double result;
 
-    @BeforeAll
-    void setUp() {
-
+    @BeforeEach
+    public void setUpClass() {
     }
 
-    @AfterEach
-    void tearDown() {
+    @AfterAll
+    public static void tearDown() {
     }
 
     @Test
-    void calculateInterestOfLoan() {
+    void calculateInterestOfLoan_paramsZero_assertZero() throws Exception {
+        amount = 0;
+        time = 0;
+
+        result = InterestCalculator.calculateInterestOfLoan(amount, time);
+
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    void calculateInterestOfLoan_amountZero_assertZero() throws Exception {
+        amount = 0;
+        time = 90;
+
+        result = InterestCalculator.calculateInterestOfLoan(amount, time);
+
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    void calculateInterestOfLoan_timeZero_assertNotZero() throws Exception {
+        amount = 10000;
+        time = 0;
+
+        result = InterestCalculator.calculateInterestOfLoan(amount, time);
+
+        Assertions.assertNotEquals(0, result);
+    }
+
+    @Test
+    void calculateInterestOfLoan_paramsNotZero_resultOverZero() throws Exception {
+        amount = 10000;
+        time = 10;
+
+        result = InterestCalculator.calculateInterestOfLoan(amount, time);
+
+        Assertions.assertTrue(result > 0);
+    }
+
+    @Test
+    void calculateInterestOfLoan_amountLessThanZero_throwsIllegalArgumentException() {
+        amount = -10;
+        time = 60;
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> InterestCalculator.calculateInterestOfLoan(amount, time));
+    }
+
+    @Test
+    void calculateInterestOfLoan_timeLessThanZero_throwsIllegalArgumentException() {
+        amount = 10000;
+        time = -10;
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> InterestCalculator.calculateInterestOfLoan(amount, time));
+    }
+
+    @Test
+    void calculateInterestOfLoan_bothArgumentsLessThanZero_throwsIllegalArgumentException() {
+        amount = -10000;
+        time = -10;
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> InterestCalculator.calculateInterestOfLoan(amount, time));
     }
 }
