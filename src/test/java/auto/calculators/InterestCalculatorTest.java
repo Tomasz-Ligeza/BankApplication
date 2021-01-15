aee441027af158717f915d61ff91a9c616ba9c40
StanMarek <stanislawmarek21@gmail.com>
2021-01-15 17:11:46 +0100

    Writing test for InterestCalculator

---
+++ src/test/java/auto/calculators/InterestCalculatorTest.java
@@ -0,0 +1,26 @@
+package auto.calculators;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InterestCalculatorTest {
+
+    private InterestCalculator calculator;
+
+    @BeforeAll
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void calculateInterestOfLoan() {
+    }
+}
